7ce6216ee0084f54ce49c1818ea2db1c17ccbbfb
MAYANK BANSAL <mayank.bansal.4381.ssgn6@gmail.com>
2026-06-24 17:03:20 +0530

    Time: 0 ms (100%), Space: 8.7 MB (7.68%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int ans=0;
+        int rem;
+        while(num>9){
+            int  ans=0;
+            while(num!=0){
+                rem=num%10;
+                num=num/10;
+                ans=ans+rem;
+            }
+            num=ans;
+        } 
+    return num;
+        
+    }
+};
