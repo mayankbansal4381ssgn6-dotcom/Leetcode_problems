17734c35ca8e22d04adf356ac083f3bf93a7515b
MAYANK BANSAL <mayank.bansal.4381.ssgn6@gmail.com>
2026-06-26 13:38:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (44.94%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int findComplement(int num) {
-        long long ans=0;
-        int rem;
-        int i;
-        long long mul=1;
-        while(num!=0){
-            rem=num%2;
-            rem=rem^1;
-            ans=rem*mul+ans;
-            num=num/2;
-            mul=mul*2;
-
-        }
-       return ans;
+        long long mask = 1;
+while (mask <= num) {
+    mask = mask << 1; 
+}
+return num ^ (mask - 1);
     }
 };
