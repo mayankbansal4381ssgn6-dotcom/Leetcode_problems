1523fc1065269620078074f7be3ece7d0b5fc81b
MAYANK BANSAL <mayank.bansal.4381.ssgn6@gmail.com>
2026-06-26 16:21:03 +0530

    Time: 0 ms (100%), Space: 13.3 MB (60.47%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int start=0;
+        int end=arr.size()-1;
+        int mid;
+        int index=arr.size(); // if array is perfectly sorted and nothing is missing
+
+        while(start<=end){
+            mid=start+(end-start)/2; // find the correct index for the kth missing element
+
+            if(arr[mid]-mid-1>=k){   // arr[mid]-mid-1 we used this to find the total numbers of positiive numbers missing . 
+            index=mid;
+            end=mid-1;
+            }
+            else{
+           
+            start=mid+1;
+            }
+
+        }
+        
+        return index+k;
+        
+        
+        
+    }
+};
