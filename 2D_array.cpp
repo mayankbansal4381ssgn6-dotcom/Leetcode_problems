457e6724490449c3133414fc6ec3eeef806f5870
MAYANK BANSAL <mayank.bansal.4381.ssgn6@gmail.com>
2026-07-04 22:08:19 +0530

    sum of antidiagonal elements

---
+++ 2D_array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int diagonalsum(int arr[][4],int rows,int col);
+int diagonalsum(int arr[][4],int rows,int col){
+    int sum=0;
+    for(int i = 0;i<rows;i++){
+        sum+=arr[i][col-1-i];
+    }
+   return sum;
+}
+
+
+int main(){
+    int arr[4][4]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+
+    cout<<diagonalsum(arr,4,4);
+    return 0;
+}
