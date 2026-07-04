b44002a33affe1d21b3f188893f97e196cf92dfd
MAYANK BANSAL <mayank.bansal.4381.ssgn6@gmail.com>
2026-07-04 22:12:06 +0530

    2D_array_diagonalsum

--- 2D_array.cpp
+++ 2D_array.cpp
@@ -4,11 +4,20 @@
 
 using namespace std;
 
+int antidiagonalsum(int arr[][4],int rows,int col);
+int antidiagonalsum(int arr[][4],int rows,int col){
+    int sum=0;
+    for(int i = 0;i<rows;i++){
+        sum+=arr[i][col-1-i];
+    }
+   return sum;
+}
+
 int diagonalsum(int arr[][4],int rows,int col);
 int diagonalsum(int arr[][4],int rows,int col){
     int sum=0;
     for(int i = 0;i<rows;i++){
-        sum+=arr[i][col-1-i];
+        sum+=arr[i][i];
     }
    return sum;
 }
@@ -17,6 +26,7 @@ int diagonalsum(int arr[][4],int rows,int col){
 int main(){
     int arr[4][4]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 
+    cout<<antidiagonalsum(arr,4,4);
     cout<<diagonalsum(arr,4,4);
     return 0;
 }
