a0cad5fddcae5646c3ee39f46453287d6089da0e
MAYANK BANSAL <mayank.bansal.4381.ssgn6@gmail.com>
2026-07-07 23:03:14 +0530

    geekforgeeks

---
+++ 2D_transpose.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    vector<vector<int>> transpose(vector<vector<int>>& mat) {
+        // code here
+        int n=mat.size();
+        //int col=mat[0].size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+        return mat;
+        
+    }
+};
