f5534314e069b9dbdd302847c3c1dd8b5f95f2d0
MAYANK BANSAL <mayank.bansal.4381.ssgn6@gmail.com>
2026-07-12 13:27:20 +0530

    Add frequencyCount function to count element frequencies
    You are given an array arr[] containing positive integers. The elements in the array arr[] range from  1 to n (where n is the size of the array), and some numbers may be repeated or absent. Your have to count the frequency of all numbers in the range 1 to n and return an array of size n such that result[i] represents the frequency of the number i (1-based indexing).
    
    Examples:
    
    Input: arr[] = [2, 3, 2, 3, 5]
    Output: [0, 2, 2, 0, 1]
    Explanation: We have: 1 occurring 0 times, 2 occurring 2 times, 3 occurring 2 times, 4 occurring 0 times, and 5 occurring 1 time.
    Input: arr[] = [3, 3, 3, 3]
    Output: [0, 0, 4, 0]
    Explanation: We have: 1 occurring 0 times, 2 occurring 0 times, 3 occurring 4 times, and 4 occurring 0 times.
    Input: arr[] = [1]
    Output: [1]
    Explanation: We have: 1 occurring 1 time, and there are no other numbers between 1 and the size of the array.
    Constraints:
    1 ≤ arr.size() ≤ 106
    1 ≤ arr[i] ≤ arr.size()
    
    Expected You are given an array arr[] containing positive integers. The elements in the array arr[] range from  1 to n (where n is the size of the array), and some numbers may be repeated or absent. Your have to count the frequency of all numbers in the range 1 to n and return an array of size n such that result[i] represents the frequency of the number i (1-based indexing).
    
    Examples:
    
    Input: arr[] = [2, 3, 2, 3, 5]
    Output: [0, 2, 2, 0, 1]
    Explanation: We have: 1 occurring 0 times, 2 occurring 2 times, 3 occurring 2 times, 4 occurring 0 times, and 5 occurring 1 time.
    Input: arr[] = [3, 3, 3, 3]
    Output: [0, 0, 4, 0]
    Explanation: We have: 1 occurring 0 times, 2 occurring 0 times, 3 occurring 4 times, and 4 occurring 0 times.
    Input: arr[] = [1]
    Output: [1]
    Explanation: We have: 1 occurring 1 time, and there are no other numbers between 1 and the size of the array.
    Constraints:
    1 ≤ arr.size() ≤ 106
    1 ≤ arr[i] ≤ arr.size()
    
    Expected

---
+++ Frequencies in a Limited Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    // Function to count the frequency of all elements from 1 to N in the array.
+    vector<int> frequencyCount(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        
+        vector<int> ans(n);
+        
+        for(int i =0;i<n;i++){
+            arr[i]--;
+            
+        }
+        
+        for(int i =0;i<n;i++){
+            arr[arr[i]%n]+=n;
+        }
+        for(int i = 0;i<n;i++){
+            //ans.push_back(arr[i]/n);
+            ans[i]=arr[i]/n;
+        }
+        return ans;
+    }
+};
